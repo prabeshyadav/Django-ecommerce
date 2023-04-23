78ce102a4fbf56ba1e81855052c220f22ea3bb4d
prabesh <prabeshyadav087@gmail.com>
2023-04-24 00:50:36 +0545

    django-ecom

--- shop/views.py
+++ shop/views.py
@@ -8,16 +8,34 @@ from math import ceil
 
 
 def index(request):
-    products =product.objects.all()
-    n=len(products)
-    nSlide=n//4+ceil((n/4)-(n//4))
-    element={'product':products,'no. of slides':nSlide,'range':range(1,nSlide)}
+# products =product.objects.all()
+    #n=products.count()
+    #n=len(products)
+    #nSlide=n//4+ceil((n/4)-(n//4))
+    #allproduct=[[products,range(1,nSlide),nSlide],
+    #          [products,range(1,nSlide),nSlide]]
+    #element={'allproducts':allproduct}
+    
+    # products= product.objects.all()
+    # n= len(products)
+    #nSlides= n//4 + ceil((n/4)-(n//4))
+    #allProds=[[products, range(1, len(products)), nSlides],[products, range(1, len(products)), nSlides]]
+    allprods=[]
+    cataprods=product.objects.values('category','id')
+    cats ={item['category'] for item in cataprods}
+    for cat in cats:
+        prod=product.objects.filter(category=cat)
+        n=len(prod)
+        nSlides=n//4 + ceil((n/4)-(n//4))
+        allprods.append([prod,range(1,nSlides),nSlides])
+    element={'allProds': allprods }
+# return render(request,"shop/index.html", )
 
     return render(request, 'shop/shop.html',element)
 
 
 def about(request):
-   return render(request,'shop/about.html')
+    return render(request,'shop/about.html')
 
 
 def contact(request):
